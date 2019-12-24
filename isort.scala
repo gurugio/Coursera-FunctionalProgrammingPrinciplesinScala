8f407e2dee7b3daca522af50a51a10ac9c3e938e
gurugio <gurugio@hanmail.net>
2019-12-24 12:22:07 +0100

    Update isort.scala

--- isort.scala
+++ isort.scala
@@ -13,3 +13,19 @@ def insert(x: Int, xs: List[Int]): List[Int] = xs match {
 
 println(insert(5, List(2,4,9)))
 println(isort(List(3,7,4,2,5,9)))
+
+def insertPair(x: (Char, Int), xs: List[(Char, Int)]): List[(Char, Int)] = xs match {
+  case List() => List(x)
+  case y :: ys => {
+    if (x._2 > y._2) y :: insertPair(x, ys)
+    else x :: xs
+  }
+}
+
+println(insert(('a',3), List(('b', 1), ('c', 2))))
+
+def pairSort(xs: List[(Char, Int)]): List[(Char, Int)] = xs mat {
+  case y :: ys => insert(y, ys) :: pairSort(ys)
+  case 
+  
+}
