8a6db537aac605cee1dddc7f4ed078ec8ddb4ba3
gurugio <gurugio@hanmail.net>
2019-12-30 11:53:18 +0100

    Update Lecture5_4.scala

--- Lecture5_4.scala
+++ Lecture5_4.scala
@@ -30,6 +30,41 @@ def flatten(xs: List[Any]): List[Any] = xs match {
 
 println(flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
 
+
+def merge(xs: List[Int], ys: List[Int]): List[Int] = xs match {
+  case List() => List()
+  case x :: xss => ys match {
+    case List() => xs
+    case y :: yss => {
+      if (x > y) y :: merge(xs, yss)
+      else x :: merge(xss, ys)
+    }
+  }
+}
+
+def merge2(xs: List[Int], ys: List[Int]): List[Int] = {
+  (xs, ys) match {
+    case (Nil, yss) => yss
+    case (xss, nil) => xss
+    case (x :: xss, y :: yss) => {
+      if (x > y) y :: merge2(xs, yss)
+      else x :: merge2(xss, ys)
+    }
+  }
+}
+
+def msort(xs: List[Int]): List[Int] = {
+  val n = xs.length/2
+  if (n == 0) xs
+  else {
+    val (fst, snd) = xs splitAt n
+    merge(msort(fst), msort(snd))
+  }
+}
+
+println(merge2(List(2,4,6), List(1,3,5)))
+println(msort(List(6,4,2,5,3,1)))
+
 val data = List('a','a','a','b','b','c','a','a')
 
 def pack[T](xs: List[T]): List[List[T]] = xs match {
