bb40071fbc1b4d05b8d647273e2983c7db45d5dd
gurugio <gurugio@hanmail.net>
2019-12-30 11:12:59 +0100

    Update Lecture5_4.scala

--- Lecture5_4.scala
+++ Lecture5_4.scala
@@ -1,3 +1,35 @@
+def init[T](xs: List[T]): List[T] = xs match {
+  case List() => throw new Error("empty")
+  case List(x) => List()
+  case y :: ys => y :: init(ys)
+}
+
+
+def reverse[T](xs: List[T]): List[T] = xs match {
+  case List() => List()
+  case y :: ys => {
+    println("y:" + y)
+    reverse(ys) ++ List(y)
+  }
+}
+println(init(List(1,2,3,4)))
+println(reverse(List(1,2,3,4,5)))
+
+def removeAt[T](n: Int, xs: List[T]): List[T] = {
+  if (n == 0) xs.tail
+  else xs.head :: removeAt(n - 1, xs.tail)
+}
+
+println(removeAt(3, List(1,2,3,4,5)))
+
+def flatten(xs: List[Any]): List[Any] = xs match {
+  case List() => List()  
+  case (y: List[Any]) :: ys => flatten(y) ++ flatten(ys)
+  case (y: Any) :: ys => y :: flatten(ys)
+}
+
+println(flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
+
 val data = List('a','a','a','b','b','c','a','a')
 
 def pack[T](xs: List[T]): List[List[T]] = xs match {
